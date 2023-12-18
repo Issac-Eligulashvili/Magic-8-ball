ff677e58c437803c1130f7fc9ee68b78861eca89
Issac-Eligulashvili <issac.eli2007@gmail.com>
2023-12-18 13:47:20 -0500

    added 8-ball logic

--- script.js
+++ script.js
@@ -11,4 +11,38 @@ function shakeMagic8Ball() {
         alert('Please enter a valid question!')
         return
     }
+
+    let randomNumber = Math.floor(Math.random() * 8);
+
+    let answer, image, color;
+
+    switch(randomNumber){
+        case 0 :
+          answer = 'It is certain'
+          break;
+        case 1 :
+          answer = 'It is decidedly so'
+          break;
+        case 2 :
+          answer = 'Reply hazy try again'
+          break;
+        case 3 :
+          answer = 'My sources say no'
+          break;
+        case 4 :
+          answer = 'Outlook not so good'
+          break;
+        case 5 :
+          answer = 'Signs point to yes'
+          break;
+        case 6 :
+          answer = 'It is certain'
+          break;
+        case 7 :
+          answer = 'It is certain'
+          break;  
+    }
+
+    const responseText = document.querySelector("#response-text")
+    responseText.innerText = answer;
 }
